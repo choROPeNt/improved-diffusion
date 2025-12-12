6125fb14bf3fca6f55869f08aa455373d2091942
choROPeNt <christian.duereth@tu-dresden.de>
2025-12-12 22:02:31 +0100

    created

---
+++ readme_hpc.md
@@ -0,0 +1,3 @@
+ml release/24.10 GCC/13.3.0 Python/3.12.3 CUDA/12.8.0 OpenMPI/5.0.3
+
+python -c "import torch;print(torch.__version__);print(torch.cuda.is_available());print(torch.cuda.get_device_name())"
---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+
+DATA_FLAGS="\
+--data_dir /data/horse/ws/dchristi-diffusion/dataset/Thinlayer\ 2-2_2023-04-28_124148/patches \
+--small_size 128 \
+--dir /data/horse/ws/dchristi-diffusion/checkpoints/super_res_128_to_256
+"
+
+MODEL_FLAGS="\
+--num_channels 192 \
+--num_res_blocks 2 \
+--learn_sigma true \
+--class_cond true \
+"
+
+DIFFUSION_FLAGS="\
+--diffusion_steps 4000 \
+--noise_schedule linear \
+--rescale_learned_sigmas false \
+--rescale_timesteps false
+"
+
+TRAIN_FLAGS="\
+--lr 3e-4 \
+--batch_size 64
+"
+
+printf "DATA_FLAGS:\n%s\n" "$DATA_FLAGS"
+printf "MODEL_FLAGS:\n%s\n" "$MODEL_FLAGS"
+printf "DIFFUSION_FLAGS:\n%s\n" "$DIFFUSION_FLAGS"
+printf "TRAIN_FLAGS:\n%s\n" "$TRAIN_FLAGS"
+
+python scripts/super_res_train.py \
+    # $DATA_FLAGS \
+    $MODEL_FLAGS \
+    $DIFFUSION_FLAGS \
+    $TRAIN_FLAGS
