6c4dc1aead0ac09ad45886385410e4a30d0c80a5
choROPeNt <christian.duereth@tu-dresden.de>
2025-12-13 16:09:10 +0100

    minor changes

--- scripts/super_res_train.py
+++ scripts/super_res_train.py
@@ -21,47 +21,47 @@ from improved_diffusion.train_util import TrainLoop
 def main():
     args = create_argparser().parse_args()
 
-    dist_util.setup_dist()
-    logger.configure()
+    # dist_util.setup_dist()
+    # logger.configure()
 
-    logger.log("creating model...")
-    model, diffusion = sr_create_model_and_diffusion(
-        **args_to_dict(args, sr_model_and_diffusion_defaults().keys())
-    )
-    model.to(dist_util.dev())
-    schedule_sampler = create_named_schedule_sampler(args.schedule_sampler, diffusion)
+    # logger.log("creating model...")
+    # model, diffusion = sr_create_model_and_diffusion(
+    #     **args_to_dict(args, sr_model_and_diffusion_defaults().keys())
+    # )
+    # model.to(dist_util.dev())
+    # schedule_sampler = create_named_schedule_sampler(args.schedule_sampler, diffusion)
 
     
 
-    logger.log("creating data loader...")
-    data = load_superres_data(
-        args.data_dir,
-        args.batch_size,
-        large_size=args.large_size,
-        small_size=args.small_size,
-        class_cond=args.class_cond,
-    )
+    # logger.log("creating data loader...")
+    # data = load_superres_data(
+    #     args.data_dir,
+    #     args.batch_size,
+    #     large_size=args.large_size,
+    #     small_size=args.small_size,
+    #     class_cond=args.class_cond,
+    # )
     
-    logger.log(f"data loader created with {type(data)} batches")
-    logger.log("training...")
+    # logger.log(f"data loader created with {type(data)} batches")
+    # logger.log("training...")
     
-    TrainLoop(
-        model=model,
-        diffusion=diffusion,
-        data=data,
-        batch_size=args.batch_size,
-        microbatch=args.microbatch,
-        lr=args.lr,
-        ema_rate=args.ema_rate,
-        log_interval=args.log_interval,
-        save_interval=args.save_interval,
-        resume_checkpoint=args.resume_checkpoint,
-        use_fp16=args.use_fp16,
-        fp16_scale_growth=args.fp16_scale_growth,
-        schedule_sampler=schedule_sampler,
-        weight_decay=args.weight_decay,
-        lr_anneal_steps=args.lr_anneal_steps,
-    ).run_loop()
+    # TrainLoop(
+    #     model=model,
+    #     diffusion=diffusion,
+    #     data=data,
+    #     batch_size=args.batch_size,
+    #     microbatch=args.microbatch,
+    #     lr=args.lr,
+    #     ema_rate=args.ema_rate,
+    #     log_interval=args.log_interval,
+    #     save_interval=args.save_interval,
+    #     resume_checkpoint=args.resume_checkpoint,
+    #     use_fp16=args.use_fp16,
+    #     fp16_scale_growth=args.fp16_scale_growth,
+    #     schedule_sampler=schedule_sampler,
+    #     weight_decay=args.weight_decay,
+    #     lr_anneal_steps=args.lr_anneal_steps,
+    # ).run_loop()
 
 
 def load_superres_data(data_dir, batch_size, large_size, small_size, class_cond=False):
@@ -99,4 +99,6 @@ def create_argparser():
 
 
 if __name__ == "__main__":
+
     main()
+
