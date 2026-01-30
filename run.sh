2ba458bde494a209f0cd68f9d79a683f7eb11818
choROPeNt <christian.duereth@tu-dresden.de>
2026-01-30 17:19:13 +0100

    reduced num_channels

--- run.sh
+++ run.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
+RUN_ID="$(date +%Y%m%d_%H%M%S)_$RANDOM"
+OUT_DIR="/data/horse/ws/dchristi-diffusion/checkpoints/CT_Hys/${RUN_ID}"
+
 DATA_FLAGS="\
 --file_list ./data/file_list.txt \
 --dataset_type hdf5 \
---dir checkpoints/test \
+--dir ${OUT_DIR} \
 "
+
 # --data_dir ./data/super_res_test/hr \
+
+
 MODEL_FLAGS="       \
 --in_channel 1      \
 --dims 3            \
---spatial_size 256  \
---num_channels 192  \
+--spatial_size 128  \
+--num_channels 64   \
 --num_res_blocks 2  \
 --learn_sigma true  \
---class_cond false   \
+--class_cond false  \
 "
 
 DIFFUSION_FLAGS="               \
